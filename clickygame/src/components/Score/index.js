9182697c226815dd19c734ec5bcfe5af49e876fb
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-01-31 16:46:43 -0500

    Updated the styling

--- clickygame/src/components/Score/index.js
+++ clickygame/src/components/Score/index.js
@@ -6,11 +6,10 @@ function Score(props) {
     <>
       <h1> Fall for Memory Game!
         <img src="./images/icon.png" alt="icon" id="icon"/> 
-      </h1>
-      
+      </h1>     
       <h3>Click on an image only once!</h3>
       <p id="message">{props.message}</p>
-      <div>
+      <div className="scoreBox">
         <span className="score">Score {props.score}</span>
         <span className="score">Top Score {props.topScore}</span>
       </div>
