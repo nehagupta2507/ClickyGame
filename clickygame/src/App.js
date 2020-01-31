0fe617e390552d566193610c35a446c5fcea8416
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-01-31 15:21:31 -0500

    Added the correct guess message

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,67 +1,61 @@
-import React, { Component } from "react";
-import Card from "./components/Card";
-import Score from "./components/Score";
-import cards from "./fall.json";
-import "./index.css"
+import React, { Component } from 'react';
+import Card from './components/Card';
+import Score from './components/Score';
+import cards from './fall.json';
+import './index.css';
 
 class App extends Component {
- // Setting this.state.friends to the friends json array
-  state = {
-    cards,
-    score: 0,
-    topScore: 0,
-    message: "Click on any image to start playing 😉",
-    };
-  
-  gameOver = () =>{
-    if (this.state.score > this.state.topScore) {
-      this.setState({topScore: this.state.score}, function() {
-        console.log(this.state.topScore);
-      });
-    }
-    this.state.cards.forEach(card => {
-      card.count = 0;
-    });
-    this.setState({score: 0, message: "Click on any image to start playing!"});
-    return true;
-  }
+	// Setting this.state.friends to the friends json array
+	state = {
+		cards,
+		score: 0,
+		topScore: 0,
+		message: 'Click on any image to start playing 😉'
+	};
 
-  clickCount = id => {
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-    this.state.cards.find((ele, i) => {
-      if (ele.id === id) {
-        if(cards[i].count === 0){
-          cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
-          //https://www.freecodecamp.org/forum/t/how-does-math-random-work-to-sort-an-array/151540/8
-          //http://www.javascriptkit.com/javatutors/arraysort.shtml
-          this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
-        } else { 
-          this.setState({score: this.state.score, message: "Nay! You lost!"});
-          setTimeout(() => {this.gameOver()}, 1000);
-        }
-      }
-    });
-  }
-  render(){
-    return (
-      <div>
-        <Score message={this.state.message} score={this.state.score} topScore={this.state.topScore} />
-        <div className="card-container">
-          {this.state.cards.map(element => (
-          <Card
-          clickCount={this.clickCount}
-          id={element.id}
-          key={element.id}
-          image={element.image}
-          />
-        ))}
-        </div>
-      </div>
-    );
-  }
+	gameOver = () => {
+		if (this.state.score > this.state.topScore) {
+			this.setState({ topScore: this.state.score }, function() {});
+		}
+		this.state.cards.forEach((card) => {
+			card.count = 0;
+		});
+		this.setState({ score: 0, message: 'Click on any image to start playing!' });
+		return true;
+	};
+
+	clickCount = (id) => {
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+		this.state.cards.find((ele, i) => {
+			if (ele.id === id) {
+				if (cards[i].count === 0) {
+					cards[i].count = cards[i].count + 1;
+					this.setState({ score: this.state.score + 1, message: "Yay, you guessed correctly!" }, function() {
+					});
+					//https://www.freecodecamp.org/forum/t/how-does-math-random-work-to-sort-an-array/151540/8
+					//http://www.javascriptkit.com/javatutors/arraysort.shtml
+					this.state.cards.sort(() => Math.random() - 0.5);
+					return true;
+				} else {
+					this.setState({ score: this.state.score, message: 'Nay! You lost!' });
+					setTimeout(() => {
+						this.gameOver();
+					}, 1000);
+				}
+			}
+		});
+	};
+	render() {
+		return (
+			<div>
+				<Score message={this.state.message} score={this.state.score} topScore={this.state.topScore} />
+				<div className="card-container">
+					{this.state.cards.map((element) => (
+						<Card clickCount={this.clickCount} id={element.id} key={element.id} image={element.image} />
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
